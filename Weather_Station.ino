7e8b4e08c5c2e4aff28f638686213a2f9391cca6
Palash Bhowmick <palash.bhowmick@gmail.com>
2022-07-11 01:00:10 +0530

    Initial commit for weather station

---
+++ Weather_Station.ino
@@ -0,0 +1,114 @@
+/*
+ * 
+ * 
+  Author: Palash Bhowmick  
+  Description: This program will collect write temperature and humidity data from DHT sensor and upload it
+  to ThingSpeak channel
+
+  Hardware: ESP8266 based boards
+
+  !!! IMPORTANT - Modify the secrets.h file for this project with your network connection and ThingSpeak channel details. !!!
+
+  Note:
+  - Requires ESP8266WiFi library and ESP8622 board add-on. See https://github.com/esp8266/Arduino for details.
+  - Select the target hardware from the Tools->Board menu
+  - This example is written for a network using WPA encryption. For WEP or WPA, change the WiFi.begin() call accordingly.
+
+  ThingSpeak ( https://www.thingspeak.com ) is an analytic IoT platform service that allows you to aggregate, visualize, and
+  analyze live data streams in the cloud. Visit https://www.thingspeak.com to sign up for a free account and create a channel.
+
+  Documentation for the ThingSpeak Communication Library for Arduino is in the README.md folder where the library was installed.
+  See https://www.mathworks.com/help/thingspeak/index.html for the full ThingSpeak documentation.
+
+  For licensing information, see the accompanying license file.
+
+  Copyright 2018, The MathWorks, Inc.
+*/
+
+#include "ThingSpeak.h"
+#include "secrets.h"
+#include <ESP8266WiFi.h>
+#include <Adafruit_Sensor.h>
+#include <DHT.h>
+#include <DHT_U.h>
+
+
+#define DHTPIN 12
+#define DHTTYPE    DHT11
+
+char ssid[] = SECRET_SSID;   // your network SSID (name)
+char pass[] = SECRET_PASS;   // your network password
+int keyIndex = 0;            // your network key Index number (needed only for WEP)
+
+WiFiClient  client;
+
+unsigned long myChannelNumber = SECRET_CH_ID;
+const char * myWriteAPIKey = SECRET_WRITE_APIKEY;
+
+DHT_Unified dht(DHTPIN, DHTTYPE);
+
+uint32_t delayMS = 30000;
+
+String myStatus = "";
+
+void setup() {
+  Serial.begin(9600);  // Initialize serial
+
+  WiFi.mode(WIFI_STA);
+  
+  ThingSpeak.begin(client);  // Initialize ThingSpeak
+}
+
+void loop() {
+
+  // Connect or reconnect to WiFi
+  if (WiFi.status() != WL_CONNECTED) {
+    Serial.print("Attempting to connect to SSID: ");
+    Serial.println(SECRET_SSID);
+    while (WiFi.status() != WL_CONNECTED) {
+      WiFi.begin(ssid, pass);  // Connect to WPA/WPA2 network. Change this line if using open or WEP network
+      Serial.print(".");
+      delay(5000);
+    }
+    Serial.println("\nConnected.");
+  }
+
+
+  // Get temperature event and print its value.
+  sensors_event_t event;
+  dht.temperature().getEvent(&event);
+  if (isnan(event.temperature)) {
+    Serial.println(F("Error reading temperature!"));
+  }
+  else {
+    Serial.print(F("Temperature: "));
+    Serial.print(event.temperature);
+    ThingSpeak.setField(1, event.temperature);//field 1: temperature in C
+    Serial.println(F("°C"));
+  }
+  // Get humidity event and print its value.
+  dht.humidity().getEvent(&event);
+  if (isnan(event.relative_humidity)) {
+    Serial.println(F("Error reading humidity!"));
+  }
+  else {
+    Serial.print(F("Humidity: "));
+    Serial.print(event.relative_humidity);
+    ThingSpeak.setField(2, event.relative_humidity);//field 2: relative humidity
+    Serial.println(F("%"));
+  }
+  
+  if (!isnan(event.temperature) && !isnan(event.relative_humidity)) {
+    // write to the ThingSpeak channel
+    int x = ThingSpeak.writeFields(myChannelNumber, myWriteAPIKey);
+    if (x == 200) {
+      Serial.println("Channel update successful.");
+    }
+    else {
+      Serial.println("Problem updating channel. HTTP error code " + String(x));
+    }
+  }
+
+  // wait for 30 sec for next update
+  delay(delayMS);
+}
